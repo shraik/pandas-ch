d49093fb98d2452796ba5f305bf2370e8e1d3ae0
shraik <shraikus@gmail.com>
2026-02-28 22:57:44 +0700

    считывание остатка

--- act.py
+++ act.py
@@ -1,10 +1,11 @@
 import pandas as pd
-
+from pathlib import Path
 import sys
 from datetime import datetime, timedelta
 
-from shared_module2 import loadvp3
+# from shared_module2 import loadvp3
 from shared_chouse import contc, save_file_data_ch, intoclickhouse, check_file_data_ch
+import configparser
 
 
 def examples():
@@ -119,9 +120,14 @@ def load_mol_excel(clumns: dict, header_row: list, filename: str) -> pd.DataFram
     возвращает фрейм с найденными колонками
     """
 
-    # res = pd.read_excel(filename, header=header_row, engine="calamine")
-    res = pd.read_excel(filename, dtype=str, header=header_row, engine="calamine")
-    res2 = pd.read_excel(filename, nrows=header_row[0], engine="calamine")
+    # res = pd.read_excel(filename, dtype=str, header=header_row, engine="calamine")
+    # res2 = pd.read_excel(filename, nrows=header_row[0], engine="calamine")
+
+    # res.to_parquet("C1_in/1с-2026-02-остатки.parquet")
+    # res2.to_parquet("C1_in/1с-2026-02-остатки_res2.parquet")
+
+    res = pd.read_parquet("C1_in/1с-2026-02-остатки.parquet")
+    res2 = pd.read_parquet("C1_in/1с-2026-02-остатки_res2.parquet")
 
     # логическая матрица поиска слова во фрейме
     res3 = res2.apply(lambda col: col.str.contains("Период", na=False), axis=1)
@@ -265,7 +271,68 @@ def makeclean_mol(filename: str, allcol=False):
     return df
 
 
+def find_latest_file(directory: str, pattern: str) -> str | None:
+    """Находит самый последний измененный файл в каталоге по заданному шаблону."""
+    try:
+        dir_path = Path(directory)
+        files = list(dir_path.glob(pattern))
+        if not files:
+            print(
+                f"Ошибка: Не найдено файлов по шаблону '{pattern}' в каталоге '{directory}'."
+            )
+            return None
+
+        latest_file = max(files, key=lambda p: p.stat().st_mtime)
+        print(
+            f"Найден самый новый файл по шаблону '{pattern}' в каталоге '{directory}':\n{latest_file}"
+        )
+        return str(latest_file)
+    except FileNotFoundError:
+        print(f"Ошибка: Каталог '{directory}' не найден.")
+        return None
+    except Exception as e:
+        print(f"Произошла непредвиденная ошибка: {e}")
+        return None
+
+
+def loadinit() -> configparser.ConfigParser:
+
+    config = configparser.ConfigParser()
+    config_file_path = "act.ini"
+
+    try:
+        with open(config_file_path, "r") as f:
+            config.read_file(f)
+    except FileNotFoundError, configparser.MissingSectionHeaderError:
+        config["DEFAULT"] = {
+            "file1": r"R:\source\python\Python-xls\data\склады\2026-02-28\все мтр на_27.02.2026.xlsx",
+            "file2": r"R:\source\python\Python-xls\data\склады\2026-02-28\Лист в ALVXXL01 (1).xlsx",
+        }
+        with open(config_file_path, "w") as configfile:
+            config.write(configfile)
+    except Exception as e:
+        print(f"An unexpected error occurred while reading the file: {e}")
+        sys.exit(1)
+    return config
+
+
 if __name__ == "__main__":
+    config_gl = loadinit()
+
+    # for key in config_gl["DEFAULT"]:
+    #     print(key)
+
+    if config_gl.has_option("DEFAULT", "file1") and config_gl.has_option(
+        "DEFAULT", "file2"
+    ):
+        file_sap1 = config_gl.get("DEFAULT", "file1")
+        file_sap2 = config_gl.get("DEFAULT", "file2")
+        print(f"File1: {file_sap1}")
+        print(f"file2: {file_sap2}")
+    else:
+        print("Не найден ключ 'file1' или 'file2' в секции 'DEFAULT'.")
+        sys.exit()
+
     db_name = "pandas"
     # client = contc()
     client = contc(db_name, hostip="192.168.5.17")
@@ -274,19 +341,76 @@ if __name__ == "__main__":
     # df_in = makeclean_mol(filets)
     # df_in.to_excel(r"data\loaded_f.xlsx")
 
-    file_sap1 = r"\\192.168.5.199\ilja\source\python\Python-xls\data\склады\2026-02-28\все мтр на_27.02.2026.xlsx"
-    df1 = pd.read_excel(file_sap1, engine="calamine", nrows=10, dtype_backend="pyarrow")
+    # df1 = pd.read_excel(file_sap1, engine="calamine", nrows=10, dtype_backend="pyarrow")
+    # df2 = pd.read_excel(file_sap2, engine="calamine", nrows=10, dtype_backend="pyarrow")
+    # # print(df1.info())
 
-    file_sap2 = r"\\192.168.5.199\ilja\source\python\Python-xls\data\склады\2026-02-28\Лист в ALVXXL01 (1).xlsx"
-    df1 = pd.read_excel(file_sap1, engine="calamine", nrows=10, dtype_backend="pyarrow")
-    df2 = pd.read_excel(file_sap2, engine="calamine", nrows=10, dtype_backend="pyarrow")
-    print(df1.info())
+    # print(df1.columns.sort_values().to_list())
+    # print(df2.columns.sort_values().to_list())
 
-    print(df1.columns.sort_values().to_list())
-    print(df2.columns.sort_values().to_list())
+    # df1 = pd.read_excel(file_sap1, engine="calamine", dtype_backend="pyarrow")
+    # df1.to_parquet("out/все мтр на_27.02.2026.parquet")
 
-    df1 = pd.read_excel(file_sap1, engine="calamine", nrows=10)
+    # df1 = pd.read_excel(file_sap2, engine="calamine", dtype_backend="pyarrow")
+    # df1.to_parquet("out/Лист в ALVXXL01 (1).parquet")
 
+    # каталоги для входных файлов
+    DATA_SAP = "SAP_in"
+    DATA_C1 = "C1_in"
+
+    print("--выбираем файл с остатками SAP---")
+    filesap = find_latest_file(DATA_SAP, "*.parquet")
+    print("--выбираем файл с остатками ОСВ---")
+    mol_file = find_latest_file(DATA_C1, "*.xlsx")
+
+    if not filesap or not mol_file:
+        print("Не удалось найти необходимые файлы данных. Выход.")
+        sys.exit(1)
+
+    print(f"--читаем SAP файл:\n{filesap}")
+    sap_ost = pd.read_parquet(filesap, dtype_backend="pyarrow")
+
+    print(f"--читаем ОСВ файл:\n{mol_file}")
+    c1_ost = load_mol_excel(
+        {
+            "Счет_": "Счет",
+            "КСМ_": "КСМ",
+            "Код склада SAP_": "Код склада SAP",
+            "Партия SAP_": "Партия SAP",
+        },
+        # [10, 11],
+        [9, 10],
+        mol_file,
+    )
+
+    c1_ost = c1_ost.dropna(subset="КСМ")
+    # c1_ost["КСМ"] = c1_ost["КСМ"].astype("int32")
+    c1_ost["КСМ"] = c1_ost["КСМ"].str.lstrip("0")
+    c1_ost["key"] = (
+        c1_ost["Код склада SAP"]
+        + c1_ost["КСМ"].astype("string[pyarrow]")
+        + c1_ost["Партия SAP"]
+    )
+
+    # pd.StringDtype(storage="pyarrow")
+    sap_ost["Материал"] = (
+        sap_ost["Материал"]
+        .astype("string[pyarrow]")
+        .convert_dtypes(dtype_backend="pyarrow")
+    )
+
+    print(sap_ost.info())
+    sap_ost["key"] = sap_ost["Склад"] + sap_ost["Материал"] + sap_ost["Партия"]
+
+    c1_ost = c1_ost.merge(sap_ost, left_on="key", right_on="key")
+
+    c1_ost.to_excel("out/c1_ost.xlsx", index=False)
+
+    # df1 = pd.read_parquet("out/все мтр на_27.02.2026.parquet", dtype_backend="pyarrow")
+    # df2 = pd.read_parquet("out/Лист в ALVXXL01 (1).parquet", dtype_backend="pyarrow")
+
+    # print(df1.info())
+    # print(df2.info())
 
 if client.ping():
     client.close()
